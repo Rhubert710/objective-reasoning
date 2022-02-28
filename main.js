03c701ab0acb47ce8d23c4abe45fa624ff1a1aa5
Rhubert710 <73670513+Rhubert710@users.noreply.github.com>
2022-02-28 15:30:53 -0500

    added functions, passed tests

--- main.js
+++ main.js
@@ -2,8 +2,60 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+//isAdmin
+function isAdmin(user)
+{
+    if (user.userRole == 'ADMIN')
+    {
+        return true;
+    }
+    return false
+}
 
+//getEmail
+function getEmail(user)
+{
+    console
+    return `${user.firstName.toLowerCase()}.${user.lastName.toLowerCase()}@codeimmersives.com`;
+}
 
+//getPlaylistLength
+function getPlaylistLength(playlist)
+{
+    return playlist.songs.length;
+}
+
+//getHardestHomework
+function getHardestHomework(arr)
+{
+    //base case for passing test
+    if (arr.length ==0)
+    {
+        return ("");
+    }
+    //start
+    let lowestAvg = arr[0];
+    for (let hw of arr)
+    {
+        if (hw.averageScore < lowestAvg.averageScore)
+        {
+            lowestAvg = hw;
+        }
+    }
+
+    return lowestAvg.name;
+}
+
+//createPhonebook
+function createPhonebook(names, numbers)
+{
+    let phoneBook = {};
+    for(let i = 0; i <names.length; i++)
+    {
+        phoneBook[names[i]] = numbers[i];
+    }
+    return phoneBook;
+}
 // ┌─────────────────────────────────────┐
 // │ Do not modify code below this line. │
 // └─────────────────────────────────────┘
